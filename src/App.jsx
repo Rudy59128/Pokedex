e04ae80d530f9de0f9787132e2c1ebb4a9ac8d6d
Rudy PLESSIS <rudy.plessis@gmail.com>
2023-03-30 17:03:59 +0200

    Intégration des props

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,20 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]}/>
     </div>
   );
 }
 
+const pokemonList = [
+  {
+      name: "Bulbasaur",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+  },
+  {
+      name: "Mew",
+  },
+];
 
 
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,30 +1,18 @@
-const pokemonList = [
-    {
-        name: "Bulbasaur",
-        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-    },
-    {
-        name: "Mew",
-    },
-];
-
-function PokemonCard() {
-    let pokemon = pokemonList[0];
-
-    if(pokemon.imgSrc) {
+function PokemonCard(props) {
+    if(props.pokemon.imgSrc) {
         return (
             <figure className="card">
-               <img src={pokemon.imgSrc}
-                    alt={pokemon.name}
+               <img src={props.pokemon.imgSrc}
+                    alt={props.pokemon.name}
                     className="card-img" />
-                <figcaption>{pokemon.name}</figcaption>
+                <figcaption>{props.pokemon.name}</figcaption>
             </figure>
         );
     } else {
         return (
             <figure className="card">
                 <p>???</p>
-                <figcaption>{pokemon.name}</figcaption>
+                <figcaption>{props.pokemon.name}</figcaption>
             </figure>
         )
     }        
